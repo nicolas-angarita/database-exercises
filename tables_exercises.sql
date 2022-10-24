7b31c5370a04751bc8dbe7df1620bd94135412b7
Nicolas Angarita <nicolas.angarita07@gmail.com>
2022-10-24 13:49:14 -0500

    Reviewed

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -14,4 +14,4 @@ SHOW DATABASES;
 
 SHOW CREATE TABLE dept_manager;
 
-dept_manager	CREATE TABLE `dept_manager` (↵  `emp_no` int NOT NULL,↵  `dept_no` char(4) NOT NULL,↵  `from_date` date NOT NULL,↵  `to_date` date NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT↵) ENGINE=InnoDB DEFAULT CHARSET=latin1
+/* dept_manager	CREATE TABLE `dept_manager` (↵  `emp_no` int NOT NULL,↵  `dept_no` char(4) NOT NULL,↵  `from_date` date NOT NULL,↵  `to_date` date NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT↵) ENGINE=InnoDB DEFAULT CHARSET=latin1*/
